11aa204745e40b123e8ef0561b766c28f79aa366
jokeejoke <69756862+jokeejoke@users.noreply.github.com>
2020-09-21 21:47:51 +0600

    Настроил mail.php, добавил модальное окно

--- js/main.js
+++ js/main.js
@@ -8,13 +8,21 @@ $(function () {
       url: "mail.php", //Change
       data: th.serialize()
     }).done(function () {
-      alert("success");
+      $(".modal_window_wrapper").css("top", "0");
+      $(".modal_window").css("visibility", "visible");
+      $(".modal_window").css("opacity", "1");
       setTimeout(function () {
         // Done Functions
         th.trigger("reset");
       }, 1000);
     });
     return false;
+  });
+
+  $(".modal_window .ok").on("click", function () {
+    $(".modal_window").css("opacity", "0");
+    $(".modal_window").css("visibility", "hidden");
+    $(".modal_window_wrapper").css("top", "-100%");
   });
 
 })
